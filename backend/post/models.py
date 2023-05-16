d47fe4ffce39d53e2eca29b80ddc82ce635bae48
Junyou Park <pjy008008@gmail.com>
2023-05-16 14:14:02 +0900

    init

---
+++ backend/post/models.py
@@ -0,0 +1,11 @@
+# Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200) # title 컬럼
+    content = models.TextField()           # content 컬럼
+    person = models.IntegerField(null=True)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.title
