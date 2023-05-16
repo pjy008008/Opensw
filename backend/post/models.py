8e34b7563f36986b5ba245891b8921f6fe00105b
Junyou Park <pjy008008@gmail.com>
2023-05-16 14:21:00 +0900

    (#2) 주석 작성

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 
+#Post model
 class Post(models.Model):
     title = models.CharField(max_length=200) # title 컬럼
     content = models.TextField()           # content 컬럼
