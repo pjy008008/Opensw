09c1cd58e18e7f0fa5eab2bf8aa76d1c6e287897
SongHojinjin <songhojin913@gmail.com>
2023-05-28 22:57:29 +0900

    Add comments : post models, serializer, views.py

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,27 +1,23 @@
-
-# Create your views here.
-# Create your views here.
 from django.shortcuts import render
 from rest_framework import generics, permissions
-
 from .models import Post
 from .serializers import PostSerializer
 
-class ListPost(generics.ListCreateAPIView): #작성
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class ListPost(generics.ListCreateAPIView): # 작성
+    queryset = Post.objects.all() # 객체 설정
+    serializer_class = PostSerializer # 직렬화 (json으로 변경)
+    permission_classes = [permissions.IsAuthenticated] # 인증된 사용자인지 확인
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer): # user 필드를 현재 사용자로 설정
         serializer.save(user=self.request.user)
 
 
-class DetailPost(generics.RetrieveUpdateDestroyAPIView): #세부정보, 수정, 삭제
+class DetailPost(generics.RetrieveUpdateDestroyAPIView): # 세부정보, 수정, 삭제
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self): #로그인 대상이 맞는지 확인
+    def get_object(self): # 로그인 대상이 맞는지 확인(user 필드 비교)
         obj = super().get_object()
         if obj.user != self.request.user:
             raise permissions.PermissionDenied
