5731a3130ea61a676327700257ad07779b778d7e
Junyou Park <pjy008008@gmail.com>
2023-05-30 23:14:11 +0900

    (#5,#29)Get요청하고 마커 구현

--- frontend/src/components/KakaoMap.js
+++ frontend/src/components/KakaoMap.js
@@ -1,38 +1,65 @@
 import React from "react";
-import { MapMarker, Map } from "react-kakao-maps-sdk";
-import { useEffect, useState } from "react";
+import { MapMarker, Map, useMap } from "react-kakao-maps-sdk";
+import { useState,useEffect } from "react";
 import styles from "./KakaoMap.module.css";
+import axios from "axios";
 
 const KakaoMap = () => {
-  // const [currentPosition, setCurrentPosition] = useState(null);
-  // useEffect(() => {
-  //   // 현재 위치를 로딩
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(
-  //       (position) => {
-  //         setCurrentPosition({
-  //           lat: position.coords.latitude,
-  //           lng: position.coords.longitude,
-  //         });
-  //       },
-  //       (error) => {
-  //         console.error(error);
-  //       }
-  //     );
-  //   } else {
-  //     console.error("Geolocation is not supported by this browser.");
-  //   }
-  // }, []);
+  const [post, setPost] = useState([]);
+  // const res = await fetch("http://127.0.0.1:8000/api/grade");
+  //       const data = await res.json();
+  //       setPosts(data);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://127.0.0.1:8000/api/", {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then(function (response) {
+        const data = response.data;
+        const posts = data.map((element) => {
+          return {
+            title: <div>{element.title}</div>,
+            latlng: { lat: element.lat, lng: element.lng },
+          };
+        });
+        setPost(posts);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  const EventMarkerContainer = ({ position, content }) => {
+    const map = useMap();
+    const [isVisible, setIsVisible] = useState(false);
 
+    return (
+      <MapMarker
+        position={position} // 마커를 표시할 위치
+        // @ts-ignore
+        onClick={(marker) => map.panTo(marker.getPosition())}
+        onMouseOver={() => setIsVisible(true)}
+        onMouseOut={() => setIsVisible(false)}
+      >
+        {isVisible && content}
+      </MapMarker>
+    );
+  };
+  console.log(post);
   return (
     <Map
       className={styles.map}
       center={{ lat: 36.627883, lng: 127.456268 }}
       level={4}
     >
-      {/* <MapMarker position={currentPosition}>
-        <div style={{ color: "#000" }}>Hello World!</div>
-      </MapMarker> */}
+      {post &&
+        post.map((value) => (
+          <EventMarkerContainer
+            key={`EventMarkerContainer-${value.latlng.lat}-${value.latlng.lng}`}
+            position={value.latlng}
+            content={value.title}
+          />
+        ))}
     </Map>
   );
 };
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,10 +1,8 @@
-import GetPost from "../components/GetPost";
 import KakaoMap from "../components/KakaoMap";
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <GetPost />
       <KakaoMap />
     </div>
   );
